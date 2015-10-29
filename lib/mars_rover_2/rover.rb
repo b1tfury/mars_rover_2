139c6214032f6606260c0932ad97add450844070
bitfurry <sahilkharb@iitj.ac.in>
2015-10-29 11:25:44 +0530

    Test Case 3 : Rover#turnRight turn the rover into right direction

--- lib/mars_rover_2/rover.rb
+++ lib/mars_rover_2/rover.rb
@@ -10,5 +10,19 @@ module MarsRover
     def position
       @position
     end
+    def turnRight
+      current_direction = @position[2]
+      if current_direction =="N"
+        current_direction = "E"
+      elsif current_direction == "S"
+        current_direction = "W"
+      elsif current_direction == "E"
+        current_direction = "S"
+      else
+        current_direction ="N"
+      end
+      @position[2] = current_direction
+      return @position
+    end
   end
 end
--- spec/mars_rover_2/rover_spec.rb
+++ spec/mars_rover_2/rover_spec.rb
@@ -10,4 +10,16 @@ module MarsRover
       expect(Rover.new([1,1,"N"]).position).to eq([1,1,"N"])
     end
   end
+  describe Rover,"#turnRight" do
+    it "should turn rover in right direction" do
+      rov =Rover.new([1,1,"N"]).turnRight
+      expect(rov).to eq([1,1,"E"])
+      rov =Rover.new([1,1,"S"]).turnRight
+      expect(rov).to eq([1,1,"W"])
+      rov =Rover.new([1,1,"E"]).turnRight
+      expect(rov).to eq([1,1,"S"])
+      rov =Rover.new([1,1,"W"]).turnRight
+      expect(rov).to eq([1,1,"N"])
+    end
+  end
 end
